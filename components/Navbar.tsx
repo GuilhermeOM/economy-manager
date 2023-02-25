e3c1726a1518ff12d8fe6cbecf1642cc3c808e8f
Guilherme Malta <guilhermeomalta0@gmail.com>
2023-02-25 16:03:56 -0300

    Adding background color to navbar and list icon for mobile or low sized screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,18 @@
-import { House, ChartBar } from "phosphor-react";
+import { House, ChartBar, List } from "phosphor-react";
 
 export const Navbar = () => {
   return (
-    <div className="w-screen h-16 p-2 px-8 inline-flex fixed top-0 left-0 right-0 items-center">
-      <div className="">
+    <div className="w-screen h-16 p-2 px-8 inline-flex fixed top-0 left-0 right-0 items-center bg-neutral-900">
+      <div className="flex items-center gap-4">
+        <List
+          size={23}
+          className="hover:text-indigo-500 transition duration-2 cursor-pointer"
+        />
         <h1 className="font-bold text-2xl bg-gradient-to-r from-indigo-500 to-blue-600 text-transparent bg-clip-text hover:from-indigo-600 hover:to-blue-800 cursor-pointer">
           ECONOMY MANAGER
         </h1>
       </div>
-      <div className="ml-auto inline-flex gap-4">
+      <div className="ml-auto hidden gap-4 md:flex">
         <House
           size={24}
           className="hover:text-indigo-500 transition duration-2 cursor-pointer"
